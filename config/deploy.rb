4201fd7f3f5b27c2de5e8f96d9c4365a0e8253e9
Jake Lim <jervie.lim@gmail.com>
2017-08-17 14:59:09 +0800

    Changes deploy settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.9.0'
+lock '3.4.0'
 
 # you can set up multiple server roles using capistrano
 server 'server', port: 7171, roles: [:web, :app, :db], primary: true
